f0a2fbe509c074dc7c2c41ef965872d31b99c0e6
Joshua Molyneux <joshuamolyneux97@hotmail.co.uk>
2022-05-30 22:17:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from config import config
 
 
 # Global variables for our requirements
-INVENTORY_REFUND_PERCENTAGE = 0.4  # 40% DECREASE (keep 60%)
+INVENTORY_REFUND_PERCENTAGE = 0.6  # 40% DECREASE (keep 60%)
 DESCALE_VALUE = 3
 
 
